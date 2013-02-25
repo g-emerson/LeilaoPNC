21d02f67ebc2b5c6f605d6c08f31a577160116c0
Gerry Emerson <gerry.con@hotmail.com>
2013-02-25 20:39:33 -0300

    testes
    testest

--- src/java/pnc/util/DateUtil.java
+++ src/java/pnc/util/DateUtil.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * Classe para operações utilitárias com Datas e horários
  * @author giovani
  */
+//testes
 public class DateUtil {
     static public Calendar getCalendarFromDateAndTime(Date date, Date time) {
         Calendar calDate = Calendar.getInstance();
